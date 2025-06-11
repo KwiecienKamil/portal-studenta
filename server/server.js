1e65e4b13f19f0aabea0cf609522eab0d99abd4e
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-06-11 22:28:07 +0200

    Loggin in with google

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+app.get("/dbrecipes", (req, res) => {
+  const sql = "SELECT * FROM recipes";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8081, () => {
+  console.log("listening");
+});
