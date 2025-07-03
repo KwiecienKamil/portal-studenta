df481cf715b0948f4dc920cf586b06df70bef6e2
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-07-03 19:20:23 +0200

    Add exam stats, fix pdf export

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,10 +1,13 @@
 import GoogleLoginBtn from "./GoogleLoginBtn";
 import logo from "../assets/logo_OT_t.png";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-[30%] lg:w-[15%] bg-accent flex flex-col justify-between text-white">
-      <img src={logo} className="max-w-[80%] mx-auto" alt="Ogarnij.to logo" />
+      <Link to="/">
+        <img src={logo} className="max-w-[80%] mx-auto" alt="Ogarnij.to logo" />
+      </Link>
       <div className="flex flex-col">
         <GoogleLoginBtn />
       </div>
